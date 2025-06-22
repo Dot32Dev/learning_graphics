a331a0ef044cc0bc0d88e2dcfbb643e0eb7c943d
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2025-06-22 18:43:58 +0800

    Clear colour

--- src/main.c
+++ src/main.c
@@ -4,17 +4,30 @@
 #include <glad/glad.h>
 
 int main(void) {
-	if (!glfwInit()) printf("Fuck");
-	GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
-	if (!window) printf("Fuck");
+	if (!glfwInit()) return -1;
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "My thing", NULL, NULL);
+	if (!window) return -2;
 
 	glfwMakeContextCurrent(window);
 	gladLoadGLLoader(glfwGetProcAddress);
 
+	glViewport(0, 0, 800, 600);
+	glClearColor(0.5, 0.5, 0.5, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window);
+
 	while (!glfwWindowShouldClose(window))
 	{
-		glfwSwapBuffers(window);
+		// glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
 	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
 }
